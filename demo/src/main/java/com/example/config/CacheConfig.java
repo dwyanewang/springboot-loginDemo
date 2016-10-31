6581165323ad3b9b26541ae1b3f6a2e4c8387b9d
timocat <snowmargin@163.com>
2016-10-31 17:36:22 +0800

    Create CacheConfig.java

---
+++ demo/src/main/java/com/example/config/CacheConfig.java
@@ -0,0 +1,14 @@
+package com.example.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+/**
+ * cacheConfig
+ *
+ */
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+}
