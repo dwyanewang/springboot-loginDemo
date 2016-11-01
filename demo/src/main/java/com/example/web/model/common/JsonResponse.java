6ad7204c46a26c47550952661c9b5df3d3e668a8
timocat <snowmargin@163.com>
2016-11-01 10:52:14 +0800

    Create JsonResponse.java

---
+++ demo/src/main/java/com/example/web/model/common/JsonResponse.java
@@ -0,0 +1,21 @@
+package com.example.web.model.common;
+
+public abstract class JsonResponse {
+    
+    private String responseCode;
+    private String message;
+    
+    
+    public String getResponseCode() {
+        return responseCode;
+    }
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
