8f6a6c8519df734e2f8590043af5adb4a7ea05ba
timocat <snowmargin@163.com>
2016-11-01 11:05:40 +0800

    Create LoginRequest.java

---
+++ demo/src/main/java/com/example/web/model/LoginRequest.java
@@ -0,0 +1,24 @@
+package com.example.web.model;
+
+import com.example.web.model.common.JsonRequest;
+
+public class LoginRequest extends JsonRequest {
+    private String loginName;
+    private String password;
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
