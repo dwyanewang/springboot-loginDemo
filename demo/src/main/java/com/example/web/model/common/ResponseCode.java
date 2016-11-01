f899e4b9deab67b3d4be41d1173cdd67675d813f
timocat <snowmargin@163.com>
2016-11-01 10:53:18 +0800

    Create ResponseCode.java

---
+++ demo/src/main/java/com/example/web/model/common/ResponseCode.java
@@ -0,0 +1,15 @@
+package com.example.web.model.common;
+
+//define some code about login
+public class ResponseCode {
+    public static final String SUCCESS = "00";
+    public static final String AUTH_FAIL = "01";
+    
+    public static final String VALIDATE_FAIL = "11";
+    
+    
+    public static final String BUSINESS_ERROR = "21";
+    
+    public static final String OTHER_ERROR = "90";
+    
+}
