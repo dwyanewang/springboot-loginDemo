0ed10e423719c57459b2dd465976fe1f018c9948
timocat <snowmargin@163.com>
2016-11-04 16:54:28 +0800

    Create AccountSummary.java

---
+++ demo/src/main/java/com/example/entity/account/AccountSummary.java
@@ -0,0 +1,14 @@
+package com.example.entity.account;
+
+public class AccountSummary {
+	private AccountSummaryDetail accountSummaries;
+
+	public AccountSummaryDetail getAccountSummaries() {
+		return accountSummaries;
+	}
+
+	public void AccountSummaryDetail(AccountSummaryDetail accountSummaries) {
+		this.accountSummaries = accountSummaries;
+	}
+
+}
